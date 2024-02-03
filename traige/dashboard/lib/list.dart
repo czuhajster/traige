f75052f515b1656f9bf6afbd361120074153d062
Moonlight3078 <mikisuzuki88@gmail.com>
2024-02-03 23:34:55 +0000

    update flutter app

--- traige/dashboard/lib/list.dart
+++ traige/dashboard/lib/list.dart
@@ -1,3 +1,5 @@
+import 'package:dashboard/List.dart';
+import 'package:dashboard/list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'dart:async';
@@ -6,19 +8,36 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'list.dart';
 
-
 // void main() {
 //   runApp(InjuryView());
 // }
 
-  class InjuryView extends StatelessWidget {
-    final List<InjuryItem> injuryList = [
-      InjuryItem(name: 'Person A', ranking: 3),
-      InjuryItem(name: 'person B', ranking: 1),
-      InjuryItem(name: 'Person C', ranking: 2),
-    ];
-    @override
-    Widget build(BuildContext context) {
+class InjuryView extends StatelessWidget {
+  static List<InjuryCase> injuryList = [
+    InjuryCase(
+      user_id: '001',
+      status: 'healthy',
+      data: InjuryData(bpm: 60, oxygen_saturation: 50, diastolic: 20, systolic: 30),
+    ),
+    InjuryCase(
+      user_id: '002',
+      status: 'dead',
+      data: InjuryData(bpm: 0, oxygen_saturation: 0, diastolic: 0, systolic: 0),
+    ),
+    InjuryCase(
+      user_id: '003',
+      status: 'healthy',
+      data: InjuryData(bpm: 66, oxygen_saturation: 55, diastolic: 22, systolic: 33),
+    ),
+    //   InjuryCase(
+    //   user_id: '002',
+    //   status: 'healthy',
+    //   data: InjuryData(bpm: 66, oxygen_saturation: 55, diastolic: 22, systolic: 33),
+    // ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -41,9 +60,16 @@ import 'list.dart';
                         ),
                       ),
                     ),
-                    child: ListTile(
-                      title: Text(injuryList[index].name),
-                      subtitle: Text('Priority: ${injuryList[index].ranking}'),
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: InjuryCaseWidget(
+                        user_id: injuryList[index].user_id,
+                        status: injuryList[index].status,
+                        bpm: injuryList[index].data.bpm,
+                        oxygen_saturation: injuryList[index].data.oxygen_saturation,
+                        diastolic: injuryList[index].data.diastolic,
+                        systolic: injuryList[index].data.systolic,
+                      ),
                     ),
                   );
                 },
@@ -56,36 +82,127 @@ import 'list.dart';
   }
 }
 
-//   return ListView.builder(
-//     padding: const EdgeInsets.all(8),
-//     itemCount: injuryList.length,
-//     itemBuilder: (BuildContext context, int index) {
-//       return Container(
-//         height: 50,
-//         color: Colors.amber,
-//         child: Center(child: Text('Entry ${injuryList[index]}')),
-//       );
-//     }
-//   );
-// }
-//   }
-
-class InjuryItem {
-  String name;
-  int ranking;
-  InjuryItem({required this.name, required this.ranking});
+InjuryCase? findCaseById(String user_id) {
+  try {
+    return InjuryView.injuryList.firstWhere((user) => user.user_id == user_id);
+  } catch (e) {
+    return null;
+  }
 }
 
-List<InjuryItem> injuryList = [
-  InjuryItem(name: 'Person A', ranking: 3),
-  InjuryItem(name: 'person B', ranking: 1),
-  InjuryItem(name: 'Person C', ranking: 2),
-];
+void createCase(String user_id, String newStatus, int newBpm, int newOxygen_saturation, int newDiastolic, int newSystolic) {
+  // Check if the user with user_id exists
+  InjuryCase? existingCase = findCaseById(user_id);
 
-// ListView.builder(
-//   itemCount: injuryList.length,
-//   itemBuilder: (BuildContext context, int index) {
-//     return InjuryListItemWidget(data: injuryList[index]);
-//   },
-// )
+  if (existingCase != null) {
+    // Case exists, update the data
+    existingCase.status = newStatus;
+    existingCase.data.bpm = newBpm;
+    existingCase.data.oxygen_saturation = newOxygen_saturation;
+    existingCase.data.diastolic = newDiastolic;
+    existingCase.data.systolic = newSystolic;
+    print('Case updated');
+  } else {
+    // Case doesn't exist, create a new one
+    InjuryCase newCase = InjuryCase(
+      user_id: user_id,
+      status: newStatus,
+      data: InjuryData(
+        bpm: newBpm,
+        oxygen_saturation: newOxygen_saturation,
+        diastolic: newDiastolic,
+        systolic: newSystolic,
+      ),
+    );
+    InjuryView.injuryList.add(newCase);
+    print('New case created');
+  }
+}
 
+class InjuryCase {
+  final String user_id;
+  String status;
+  InjuryData data;
+
+  InjuryCase({
+    required this.user_id,
+    required this.status,
+    required this.data,
+  });
+}
+
+class InjuryData {
+  int bpm;
+  int oxygen_saturation;
+  int diastolic;
+  int systolic;
+
+  InjuryData({
+    required this.bpm,
+    required this.oxygen_saturation,
+    required this.diastolic,
+    required this.systolic,
+  });
+}
+
+class InjuryCaseWidget extends StatelessWidget {
+  final String user_id;
+  String status;
+  int bpm;
+  int oxygen_saturation;
+  int diastolic;
+  int systolic;
+
+  InjuryCaseWidget({
+    required this.user_id,
+    required this.status,
+    required this.bpm,
+    required this.oxygen_saturation,
+    required this.diastolic,
+    required this.systolic,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Icon(Icons.person),
+              SizedBox(width: 10), // Add some spacing between the icon and text
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Injury ID: $user_id, Status: $status',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('BPM: $bpm, Saturation: $oxygen_saturation, Diastolic: $diastolic, Systolic: $systolic'),
+          ),
+        ],
+      ),
+          );
+    //       Align(
+    //         alignment: Alignment.centerLeft,
+    //         child: Text(
+    //           'User ID: $user_id, Status: $status',
+    //           style: TextStyle(fontWeight: FontWeight.bold),
+    //         ),
+    //       ),
+    //       Align(
+    //         alignment: Alignment.centerLeft,
+    //         child: Text('BPM: $bpm, Saturation: $oxygen_saturation, Diastolic: $diastolic, Systolic: $systolic'),
+    //       ),
+    //     ],
+    //   ),
+    // );
+  }
+}
--- traige/dashboard/test/widget_test.dart
+++ traige/dashboard/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:med/main.dart';
+import 'package:dashboard/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
