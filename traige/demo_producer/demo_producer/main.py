2d89338cf243a05552a5194ec0146a0168e8cdc0
czuhajster <55452766+czuhajster@users.noreply.github.com>
2024-02-03 22:18:14 +0000

    DemoProducer: Read vital data from the dummy CSV file.

--- traige/demo_producer/demo_producer/main.py
+++ traige/demo_producer/demo_producer/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+import csv
+
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI(
     title="Demo TrAIge Producer",
     description="Demo TrAIge Producer",
 )
+
+csv_reader = None
+current_row_index = 0
+
+def get_csv_reader():
+    global csv_reader
+    if csv_reader is None:
+        with open("dead-person.csv") as file:
+            csv_reader = list(csv.DictReader(file))
+    return csv_reader
 
 @app.get("/")
 async def get_root():
@@ -12,6 +25,11 @@ async def get_root():
 
 @app.get("/v2/body")
 async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_webhook: bool = False, with_samples: bool = False):
+    global current_row_index
+    reader = get_csv_reader()
+    if current_row_index < len(reader):
+        row = reader[current_row_index]
+        current_row_index += 1
         response = {
             "status": "success",
             "type": "body",
@@ -33,8 +51,8 @@ async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_we
                 "blood_pressure_data": {
                     "blood_pressure_samples": [
                     {
-                        "diastolic_bp": 81.56516432156914,
-                        "systolic_bp": 148.87580323151633,
+                        "diastolic_bp": row['diastolic'],
+                        "systolic_bp": row['systolic'],
                         "timestamp": "2024-02-03T17:59:01.023000+00:00"
                     },
                     ]
@@ -46,7 +64,7 @@ async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_we
                     "detailed": {
                         "hr_samples": [
                         {
-                            "bpm": 122,
+                            "bpm": row['bpm'],
                             "context": 0,
                             "timestamp": "2024-02-03T17:59:01.023000+00:00"
                         },
@@ -73,7 +91,7 @@ async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_we
                     "avg_saturation_percentage": 99,
                     "saturation_samples": [
                     {
-                        "percentage": 91,
+                        "percentage": row['oxygen saturation percentage'],
                         "timestamp": "2024-02-03T17:59:01.023000+00:00",
                         "type": 0
                     }
@@ -91,6 +109,87 @@ async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_we
                 }
                 }
             ],
-        }
+        }    
+        return response
+    else:
+        response = {
+            "status": "success",
+            "type": "body",
+            "user": {
+                "active": True,
+                "created_at": None,
+                "last_webhook_update": None,
+                "provider": "APPLE",
+                "reference_id": "",
+                "scopes": None,
+                "user_id": user_id
+            },
+            "data": [
+                {
+                "metadata": {
+                    "end_time": "2024-02-03T18:44:01.023000+00:00",
+                    "start_time": "2024-02-03T17:59:01.023000+00:00"
+                },
+                "blood_pressure_data": {
+                    "blood_pressure_samples": [
+                    {
+                        "diastolic_bp": 0,
+                        "systolic_bp": 0,
+                        "timestamp": "2024-02-03T17:59:01.023000+00:00"
+                    },
+                    ]
+                },
+                "heart_data": {
+                    "afib_classification_samples": [],
+                    "ecg_signal": [],
+                    "heart_rate_data": {
+                    "detailed": {
+                        "hr_samples": [
+                        {
+                            "bpm": 0,
+                            "context": 0,
+                            "timestamp": "2024-02-03T17:59:01.023000+00:00"
+                        },
+                        ],
+                        "hrv_samples_rmssd": [],
+                        "hrv_samples_sdnn": []
+                    },
+                    "summary": {
+                        "avg_hr_bpm": 120.18388352021609,
+                        "avg_hrv_rmssd": None,
+                        "avg_hrv_sdnn": 84.2635069166341,
+                        "hr_zone_data": [],
+                        "max_hr_bpm": 50.26205196642002,
+                        "min_hr_bpm": 105.71794817044378,
+                        "resting_hr_bpm": None,
+                        "user_max_hr_bpm": None
+                    }
+                    },
+                    "pulse_wave_velocity_samples": [],
+                    "rr_interval_samples": []
+                },
               
+                "oxygen_data": {
+                    "avg_saturation_percentage": 99,
+                    "saturation_samples": [
+                    {
+                        "percentage": 0,
+                        "timestamp": "2024-02-03T17:59:01.023000+00:00",
+                        "type": 0
+                    }
+                    ],
+                },
+                "temperature_data": {
+                    "ambient_temperature_samples": [],
+                    "body_temperature_samples": [
+                    {
+                        "temperature_celsius": 35.05331429501405,
+                        "timestamp": "2024-02-03T17:59:01.023000+00:00"
+                    },
+                    ],
+                    "skin_temperature_samples": []
+                }
+                }
+            ],
+        }    
         return response
