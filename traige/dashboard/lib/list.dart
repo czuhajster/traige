41e431ec161ed40cf7501cb28eb640befdcf12dc
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2024-02-04 05:15:03 +0000

    list.dart websocket connection

--- traige/dashboard/lib/list.dart
+++ traige/dashboard/lib/list.dart
@@ -1,198 +1,8 @@
-import 'package:dashboard/List.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:web_socket_channel/web_socket_channel.dart';
-import 'package:http/http.dart' as http;
-import 'list.dart';
 
-// void main() {
-//   runApp(InjuryView());
-// }
-
-// void main() {
-
-// }
-
-const user1 = {
-'user_id': '001',
-'status': 'healthy',
-  'data': {
-  'bpm': 60,
-  'oxygen_saturation': 50,
-  'diastolic': 20,
-  'systolic': 30,
-  },
-  'confidence': 10,
-};
-
-const user2 = {
-'user_id': '002',
-'status': 'healthy',
-  'data': {
-  'bpm': 66,
-  'oxygen_saturation': 55,
-  'diastolic': 22,
-  'systolic': 33,
-  },
-'confidence': 70,
-};
-
-const user3 = {
-'user_id': '003',
-'status': 'unhealthy',
-  'data': {
-  'bpm': 66,
-  'oxygen_saturation': 55,
-  'diastolic': 22,
-  'systolic': 33,
-  },
-  'confidence': 50,
-};
-
-const user4 = {
-'user_id': '004',
-'status': 'healthy',
-  'data': {
-  'bpm': 66,
-  'oxygen_saturation': 55,
-  'diastolic': 22,
-  'systolic': 33,
-  },
-  'confidence': 99,
-};
-
-
-
-class InjuryView extends StatelessWidget {
-
-  static List<InjuryCase> injuryList = [
-
-  ];
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Injury List'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: injuryList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.grey,
-                          width: 1.0,
-                        ),
-                      ),
-                    ),
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      child: InjuryCaseWidget(
-                        user_id: injuryList[index].user_id,
-                        status: injuryList[index].status,
-                        bpm: injuryList[index].data.bpm,
-                        oxygen_saturation: injuryList[index].data.oxygen_saturation,
-                        diastolic: injuryList[index].data.diastolic,
-                        systolic: injuryList[index].data.systolic,
-                        confidence: injuryList[index].confidence,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-void connectWebSocket() {
-  final channel = WebSocketChannel.connect(
-    Uri.parse('wss://traige-ws.saajan.net/ws'),
-  );
-  channel.stream.listen((message) {
-    print('Received: $message');
-    processReceivedData(message);
-  }, onDone: () {
-    print('Channel closed');
-  }, onError: (error) {
-    print('Websocket Error: $error');
-  });
-  
-}
-
-void processReceivedData(String message) {
-  try {
-    Map<String, dynamic> jsonData = json.decode(message);
-    createCase(jsonData);
-  } catch (e) {
-    print('Error processing received data: $e');
-  }
-}
-
-InjuryCase? findCaseById(String user_id) {
-  try {
-    return InjuryView.injuryList.firstWhere((user) => user.user_id == user_id);
-  } catch (e) {
-    return null;
-  }
-}
-
-void createCase(jsonData) {
-  // Check if the user with user_id exists
-  String user_id = jsonData['user_id'];
-  String newStatus = jsonData['status'];
-  int newBpm = jsonData['bpm'];
-  int newOxygen_saturation = jsonData['oxygen_saturation'];
-  int newDiastolic = jsonData['diastolic'];
-  int newSystolic = jsonData['systolic'];
-  int confidence = jsonData['confidence'];
-  InjuryCase? existingCase = findCaseById(user_id);
-
-  if (existingCase != null) {
-    // Case exists, update the data
-    existingCase.status = newStatus;
-    existingCase.data.bpm = newBpm;
-    existingCase.data.oxygen_saturation = newOxygen_saturation;
-    existingCase.data.diastolic = newDiastolic;
-    existingCase.data.systolic = newSystolic;
-    existingCase.confidence = confidence;
-    print('Case updated');
-  } else {
-    // Case doesn't exist, create a new one
-    InjuryCase newCase = InjuryCase(
-      user_id: user_id,
-      status: newStatus,
-      data: InjuryData(
-        bpm: newBpm,
-        oxygen_saturation: newOxygen_saturation,
-        diastolic: newDiastolic,
-        systolic: newSystolic,
-      ),
-      confidence: confidence,
-    );
-    InjuryView.injuryList.add(newCase);
-    print('New case created');
-
-    InjuryView.injuryList.sort((a, b) => b.confidence.compareTo(a.confidence));
-  }
-}
-
+// Define the InjuryCase and InjuryData classes outside of any Widget to ensure they are accessible globally
 class InjuryCase {
   final String user_id;
   String status;
@@ -221,77 +31,120 @@ class InjuryData {
   });
 }
 
-class InjuryCaseWidget extends StatelessWidget {
-  final String user_id;
-  String status;
-  int bpm;
-  int oxygen_saturation;
-  int diastolic;
-  int systolic;
-  int confidence;
+// InjuryView StatefulWidget
+class InjuryView extends StatefulWidget {
+  @override
+  _InjuryViewState createState() => _InjuryViewState();
+}
 
-  InjuryCaseWidget({
-    required this.user_id,
-    required this.status,
-    required this.bpm,
-    required this.oxygen_saturation,
-    required this.diastolic,
-    required this.systolic,
-    required this.confidence,
-  });
+class _InjuryViewState extends State<InjuryView> {
+  late final WebSocketChannel channel;
+  List<InjuryCase> injuryList = []; // injuryList is now a non-static member of _InjuryViewState
+
+  @override
+  void initState() {
+    super.initState();
+    connectWebSocket();
+    
+  }
+
+  void connectWebSocket() {
+    channel = WebSocketChannel.connect(
+      Uri.parse('wss://traige-ws.saajan.net/ws'),
+    );
+
+    channel.stream.listen((message) {
+      print('Received: $message');
+      processReceivedData(message);
+    }, onDone: () {
+      print('Channel closed');
+    }, onError: (error) {
+      print('Websocket Error: $error');
+    });
+  }
+
+  void processReceivedData(String message) {
+    setState(() {
+      try {
+        Map<String, dynamic> jsonData = json.decode(message);
+        createCase(jsonData);
+      } catch (e) {
+        print('Error processing received data: $e');
+      }
+    });
+  }
+
+void createCase(Map<String, dynamic> jsonData) {
+  String user_id = jsonData['user_id'];
+  InjuryCase? existingCase;
+
+  try {
+    existingCase = injuryList.firstWhere(
+      (caseItem) => caseItem.user_id == user_id,
+    );
+  } catch (e) {
+    print('Error finding existing case: $e');
+  }
+
+  if (existingCase != null) {
+    // Update the existing case...
+    existingCase.status = jsonData['status'];
+    existingCase.data = InjuryData(
+      bpm: jsonData['data']['bpm'],
+      oxygen_saturation: jsonData['data']['oxygen_saturation'],
+      diastolic: jsonData['data']['diastolic'],
+      systolic: jsonData['data']['systolic'],
+    );
+    existingCase.confidence = jsonData['confidence'];
+  } else {
+    // Create a new case and add it to the list...
+    InjuryCase newCase = InjuryCase(
+      user_id: user_id,
+      status: jsonData['status'],
+      data: InjuryData(
+        bpm: jsonData['data']['bpm'],
+        oxygen_saturation: jsonData['data']['oxygen_saturation'],
+        diastolic: jsonData['data']['diastolic'],
+        systolic: jsonData['data']['systolic'],
+      ),
+      confidence: jsonData['confidence'],
+    );
+    injuryList.add(newCase);
+  }
+
+  // Sort the list based on confidence, highest first.
+  injuryList.sort((a, b) => b.confidence.compareTo(a.confidence));
+}
+
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Icon(Icons.person),
-              SizedBox(width: 10), // Add some spacing between the icon and text
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Injury ID: $user_id, Status: $status Confidence: $confidence',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text('BPM: $bpm, Saturation: $oxygen_saturation, Diastolic: $diastolic, Systolic: $systolic'),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Injury List'),
       ),
-          );
-
+      body: Center(
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: injuryList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = injuryList[index];
+            return ListTile(
+              title: Text('Injury ID: ${item.user_id}, Status: ${item.status}, Confidence: ${item.confidence}'),
+              subtitle: Text('BPM: ${item.data.bpm}, Saturation: ${item.data.oxygen_saturation}, Diastolic: ${item.data.diastolic}, Systolic: ${item.data.systolic}'),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
 
-
-void sortInjuryList() {
-  // Custom sorting function based on health status and confidence
-  InjuryView.injuryList.sort((a, b) =>
-    b.confidence.compareTo(a.confidence)
-
-  );
-}
-
-int _compareStatus(String statusA, String statusB) {
-  // Define the order: unhealthy comes first, followed by healthy, and then dead
-  if (statusA == 'unhealthy' && statusB != 'unhealthy') {
-    return -1;
-  } else if (statusA != 'unhealthy' && statusB == 'unhealthy') {
-    return 1;
-  } else if (statusA == 'healthy' && statusB != 'healthy') {
-    return -1;
-  } else if (statusA != 'healthy' && statusB == 'healthy') {
-    return 1;
-  } else {
-    return 0;
-  }
-}
+void main() => runApp(MaterialApp(home: InjuryView()));
--- traige/dashboard/lib/main.dart
+++ traige/dashboard/lib/main.dart
@@ -15,21 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -54,12 +39,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
