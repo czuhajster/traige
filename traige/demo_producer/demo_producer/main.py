a479d6e932feda6b96df2331a4b3bcae6dad73ef
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2024-02-04 06:59:20 +0000

    cool files

--- traige/demo_producer/demo_producer/main.py
+++ traige/demo_producer/demo_producer/main.py
@@ -14,6 +14,7 @@ app = FastAPI(
     description="Demo TrAIge Producer",
 )
 
+
 @app.get("/")
 async def get_root():
     return f"Hello"
@@ -26,7 +27,13 @@ async def get_body(user_id: str, new_state: str):
 
 
 @app.get("/v2/body")
-async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_webhook: bool = False, with_samples: bool = False):
+async def get_body(
+    user_id: str,
+    start_date: str = "",
+    end_date: str = "",
+    to_webhook: bool = False,
+    with_samples: bool = False,
+):
     global SOLDIER_STATE
     if not SOLDIER_STATE.get(user_id):
         SOLDIER_STATE[user_id] = "healthy"
@@ -44,74 +51,75 @@ async def get_body(user_id: str, start_date: str = "", end_date: str = "", to_we
             "provider": "APPLE",
             "reference_id": "",
             "scopes": None,
-            "user_id": user_id
+            "user_id": user_id,
         },
         "data": [
             {
-            "metadata": {
-                "end_time": "2024-02-03T18:44:01.023000+00:00",
-                "start_time": "2024-02-03T17:59:01.023000+00:00"
-            },
-            "blood_pressure_data": {
-                "blood_pressure_samples": [
-                {
-                    "diastolic_bp": generated_data[0],
-                    "systolic_bp": generated_data[1],
-                    "timestamp": "2024-02-03T17:59:01.023000+00:00"
+                "metadata": {
+                    "end_time": "2024-02-03T18:44:01.023000+00:00",
+                    "start_time": "2024-02-03T17:59:01.023000+00:00",
                 },
-                ]
-            },
-            "heart_data": {
-                "afib_classification_samples": [],
-                "ecg_signal": [],
-                "heart_rate_data": {
-                "detailed": {
-                    "hr_samples": [
-                    {
-                        "bpm": generated_data[2],
-                        "context": 0,
-                        "timestamp": "2024-02-03T17:59:01.023000+00:00"
+                "blood_pressure_data": {
+                    "blood_pressure_samples": [
+                        {
+                            "diastolic_bp": generated_data[0],
+                            "systolic_bp": generated_data[1],
+                            "timestamp": "2024-02-03T17:59:01.023000+00:00",
+                        },
+                    ]
+                },
+                "heart_data": {
+                    "afib_classification_samples": [],
+                    "ecg_signal": [],
+                    "heart_rate_data": {
+                        "detailed": {
+                            "hr_samples": [
+                                {
+                                    "bpm": generated_data[2],
+                                    "context": 0,
+                                    "timestamp": (
+                                        "2024-02-03T17:59:01.023000+00:00"
+                                    ),
+                                },
+                            ],
+                            "hrv_samples_rmssd": [],
+                            "hrv_samples_sdnn": [],
+                        },
+                        "summary": {
+                            "avg_hr_bpm": 120.18388352021609,
+                            "avg_hrv_rmssd": None,
+                            "avg_hrv_sdnn": 84.2635069166341,
+                            "hr_zone_data": [],
+                            "max_hr_bpm": 50.26205196642002,
+                            "min_hr_bpm": 105.71794817044378,
+                            "resting_hr_bpm": None,
+                            "user_max_hr_bpm": None,
+                        },
                     },
+                    "pulse_wave_velocity_samples": [],
+                    "rr_interval_samples": [],
+                },
+                "oxygen_data": {
+                    "avg_saturation_percentage": 99,
+                    "saturation_samples": [
+                        {
+                            "percentage": generated_data[3],
+                            "timestamp": "2024-02-03T17:59:01.023000+00:00",
+                            "type": 0,
+                        }
                     ],
-                    "hrv_samples_rmssd": [],
-                    "hrv_samples_sdnn": []
                 },
-                "summary": {
-                    "avg_hr_bpm": 120.18388352021609,
-                    "avg_hrv_rmssd": None,
-                    "avg_hrv_sdnn": 84.2635069166341,
-                    "hr_zone_data": [],
-                    "max_hr_bpm": 50.26205196642002,
-                    "min_hr_bpm": 105.71794817044378,
-                    "resting_hr_bpm": None,
-                    "user_max_hr_bpm": None
-                }
+                "temperature_data": {
+                    "ambient_temperature_samples": [],
+                    "body_temperature_samples": [
+                        {
+                            "temperature_celsius": 35.05331429501405,
+                            "timestamp": "2024-02-03T17:59:01.023000+00:00",
+                        },
+                    ],
+                    "skin_temperature_samples": [],
                 },
-                "pulse_wave_velocity_samples": [],
-                "rr_interval_samples": []
-            },
-            
-            "oxygen_data": {
-                "avg_saturation_percentage": 99,
-                "saturation_samples": [
-                {
-                    "percentage": generated_data[3],
-                    "timestamp": "2024-02-03T17:59:01.023000+00:00",
-                    "type": 0
-                }
-                ],
-            },
-            "temperature_data": {
-                "ambient_temperature_samples": [],
-                "body_temperature_samples": [
-                {
-                    "temperature_celsius": 35.05331429501405,
-                    "timestamp": "2024-02-03T17:59:01.023000+00:00"
-                },
-                ],
-                "skin_temperature_samples": []
-            }
             }
         ],
-    }    
+    }
     return response
